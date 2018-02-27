529a9c4f4d56c794e09a72e9c8d668d9d637efbe
GlennGeelen <g.geelen@defacto.nl>
2018-02-27 11:27:58 +0100

    Add the active option :exact_with_params
    this will add the query string to the request_path and checks if the to and new request path are equal.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,6 +5,7 @@ defmodule TestHelpers do
     %Plug.Conn{}
     |> Map.put(:private, make_private(opts))
     |> Map.put(:request_path, opts[:path])
+    |> Map.put(:query_string, opts[:query_string] || "")
   end
 
   defp make_private(opts) do
